0655e09dbdcffcb16d6a40932556be02120f63e2
ZidanVian <90166800+ZidanVian@users.noreply.github.com>
2022-02-28 20:14:30 +0700

    Add files via upload

---
+++ No 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int i, n, data[50];
+    float rata,jum;
+    
+    cout << "Banyaknya Data = ";
+    cin >> n;
+    cout << endl;
+    
+    for (i=0; i<n; i++)
+        {
+           cout << "Data" << "[" << i+1 << "]" << " = ";
+           cin >> data[i];
+           jum += data[i];
+        }
+        
+    rata=jum/n;
+        
+    cout << endl;
+    cout << "Banyaknya data = " << n << endl;
+    cout << "Rata-rata = " << rata << endl;
+    cout << "Jumlah = " << jum << endl;
+    
+  	getch();
+}
